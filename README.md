c88f0cf02723d53814c606ae44c5a8b549cdb62b
Bryan Louie Martinez <>
2023-10-30 22:05:25 +0800

    Add README, bootstrap

---
+++ README.md
@@ -0,0 +1,22 @@
+# Introduction
+
+This is a DSA problem, and it requires DSA knowledge (commonly taught in CS courses).
+
+If you are not familiar with data structures and algorithms, you can refer to the repository [here](https://github.com/brymartinez/data-structures-and-algorithms).
+
+If you prefer to check out the linear data structure solution which is more memory-intensive, you can refer to the repository [here](https://github.com/brymartinez/family-tree).
+
+# Problem
+Our problem is set on the planet of Lengaburu, the distant galaxy of Tara B. Our protagonists are King Arthur, Queen Margaret and their family.
+
+King Arthur is the emperor of Lengaburu and has been ruling the planet for the last 350 
+years (they have long lives in Lengaburu!). Let’s write some code to get to know the family.
+
+The solution should model out the King Arthur family tree so that:
+- You should be able to add a child to any family in the tree through the mother
+- Given a ‘name’ and a ‘relationship’, you should output the people corresponding to the relationship in the order in which they are added to the family tree
+
+Simple right? Remember, our evaluation is based not only on getting the right output, but on how you come up with the solution altogether.
+
+# Pending Tasks
+ - Create unit tests via Jest
---
+++ src/utils/bootstrap.ts
@@ -0,0 +1,138 @@
+import { Person } from '../relationships/relationship';
+import { Tree } from '../data-structure/tree';
+
+export const bootstrap = (tree: Tree<Person>) => {
+  const d0: Tree<Person>[] = [];
+  const d1: Tree<Person>[] = [];
+  const d2: Tree<Person>[] = [];
+  const d3: Tree<Person>[] = [];
+
+  d0.push(
+    tree.insert({
+      name: 'Queen Margret',
+      gender: 'Female',
+      spouse: { name: 'King Arthur', gender: 'Male' },
+    }),
+  );
+
+  d1.push(
+    tree.insert({
+      name: 'Bill',
+      gender: 'Male',
+      spouse: { name: 'Flora', gender: 'Female' },
+    }),
+    tree.insert({
+      name: 'Charlie',
+      gender: 'Male',
+    }),
+    tree.insert({
+      name: 'Percy',
+      gender: 'Male',
+      spouse: {
+        name: 'Audrey',
+        gender: 'Female',
+      },
+    }),
+    tree.insert({
+      name: 'Ronald',
+      gender: 'Male',
+      spouse: {
+        name: 'Helen',
+        gender: 'Female',
+      },
+    }),
+    tree.insert({
+      name: 'Ginerva',
+      gender: 'Female',
+      spouse: {
+        name: 'Harry',
+        gender: 'Male',
+      },
+    }),
+  );
+
+  d2.push(
+    d1[0].insert({
+      name: 'Victoire',
+      gender: 'Female',
+      spouse: { name: 'Ted', gender: 'Male' },
+    }),
+    d1[0].insert({
+      name: 'Dominique',
+      gender: 'Female',
+    }),
+    d1[0].insert({
+      name: 'Louis',
+      gender: 'Male',
+    }),
+    d1[2].insert({
+      name: 'Molly',
+      gender: 'Female',
+    }),
+    d1[2].insert({
+      name: 'Lucy',
+      gender: 'Female',
+    }),
+    d1[3].insert({
+      name: 'Rose',
+      gender: 'Female',
+      spouse: {
+        name: 'Malfoy',
+        gender: 'Male',
+      },
+    }),
+    d1[3].insert({
+      name: 'Hugo',
+      gender: 'Male',
+    }),
+    d1[4].insert({
+      name: 'James',
+      gender: 'Male',
+      spouse: {
+        name: 'Darcy',
+        gender: 'Female',
+      },
+    }),
+    d1[4].insert({
+      name: 'Albus',
+      gender: 'Male',
+      spouse: {
+        name: 'Alice',
+        gender: 'Female',
+      },
+    }),
+    d1[4].insert({
+      name: 'Lily',
+      gender: 'Female',
+    }),
+  );
+
+  d3.push(
+    d2[0].insert({
+      name: 'Remus',
+      gender: 'Male',
+    }),
+    d2[5].insert({
+      name: 'Draco',
+      gender: 'Male',
+    }),
+    d2[5].insert({
+      name: 'Aster',
+      gender: 'Female',
+    }),
+    d2[7].insert({
+      name: 'William',
+      gender: 'Male',
+    }),
+    d2[8].insert({
+      name: 'Ron',
+      gender: 'Male',
+    }),
+    d2[8].insert({
+      name: 'Ginny',
+      gender: 'Female',
+    }),
+  );
+
+  return tree;
+};
